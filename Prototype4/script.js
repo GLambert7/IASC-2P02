f3082f67dc04a9117cccf41a5b335470cf021180
GLambert7 <>
2025-03-10 01:51:43 -0400

    fixed dataviz 1

--- Prototype4/script.js
+++ Prototype4/script.js
@@ -47,7 +47,7 @@ const camera = new THREE.PerspectiveCamera(
     100
 )
 scene.add(camera)
-camera.position.set (5, 5, 5)
+camera.position.set (0, 12, -20)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -61,27 +61,96 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 /***********
+** Lights **
+***********/
+//directional light
+const directionalLight = new THREE.DirectionalLight(0x404040, 100)
+scene.add(directionalLight)
+
+/***********
 ** MESHES **
 ***********/
 
-// sphere
-const sphereGeo = new THREE.SphereGeometry(1); 
-const sphereMat = new THREE.MeshNormalMaterial(); 
-const sphere = new THREE.Mesh( sphereGeo, sphereMat );
-scene.add( sphere );
-sphere.position.set(5, 5, 5)
+// Cube Geometry
+const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
 
-const sphereGeo2 = new THREE.SphereGeometry(1); 
-const sphereMat2 = new THREE.MeshNormalMaterial(); 
-const sphere2 = new THREE.Mesh( sphereGeo2, sphereMat2 );
-scene.add( sphere2 );
-sphere2.position.set(3, 3, 3)
+const drawCube = (height, color) =>
+{
+    //create cube material
+    const material = new THREE.MeshStandardMaterial({
+        color: new THREE.Color(color)
+    })
+
+    //Create cube
+    const cube = new THREE.Mesh(boxGeometry, material)
+
+    //position cube
+    cube.position.x = (Math.random() - 0.5) * 10
+    cube.position.z = (Math.random() - 0.5) * 10
+    cube.position.y = height - 10
+
+    //random cube rotation
+    cube.rotation.x = Math.random() * 2 * Math.PI
+    cube.rotation.y = Math.random() * 2 * Math.PI
+    cube.rotation.z = Math.random() * 2 * Math.PI
+
+    //add cube to scene
+    scene.add(cube)
+}
+
+//drawCube(0, 'red')
+//drawCube(1, 'green')
+//drawCube(2, 'yellow')
+//drawCube(3, 'blue')
 
 /*******
 ** UI **
 *******/
 // Ui
 const ui = new dat.GUI()
+
+/*******************
+** Text Analysis **
+*******************/
+//SourceText
+const sourceText = "In a mushroom village lived a wizard hedgehog named Carl. Carl lived peacfully in his village, he loved selling potions, and helping people out with any magical problems. One fatefull day the village was under attack by a gnome army who brought their dragon! Being a peacfull mushroom civilization, they had no wariors to defend themselves and it looked as if the village was going to be destroyed. From within the ruins Carl emerged, having trained under a saint class mage Carl knew enough defensive and offensive spells to save the village. After defeating the dragon Carl was regarded as a hero and the village prospered hapily ever after."
+
+//variables
+let parsedText, tokenizedText
+
+//Parse and Tokenize SourceText
+const tokenizeSourceText = () =>
+{
+    //strip periods and downcase source text
+    parsedText = sourceText.replaceAll(".", "").toLowerCase()
+    //tokenize text
+    tokenizedText = parsedText.split(/[^\w']+/)
+    
+}
+
+//find seatchedTerm in tokenizedText
+const findSearchTermInTokenizedText = (term, color) => 
+{
+    for(let i = 0; i < tokenizedText.length; i++)
+    {
+        //if tokenized text matches our search term
+        if(tokenizedText[i] === term){
+            //convert i into height, which is a value between 0 and 20
+            const height = (100 / tokenizedText.length) * i * 0.2
+            
+            //call drawCuube function 100 times using converted height value
+            for(let a = 0; a < 100; a++)
+            drawCube(height, color)
+        }
+    }
+}
+
+tokenizeSourceText()
+findSearchTermInTokenizedText('mushroom', 'red')
+findSearchTermInTokenizedText('village', 'white')
+findSearchTermInTokenizedText("dragon", "black")
+findSearchTermInTokenizedText("carl", "green")
+
 
 /*******************
 ** ANIMATION LOOP **
@@ -92,15 +161,7 @@ const animation = () => {
     // return elapsed time
     const elapsedTime = clock.getElapsedTime()
 
-    sphere2.position.z = Math.sin(elapsedTime)
-    sphere2.position.x = Math.sin(elapsedTime)
-    sphere2.position.y = Math.sin(elapsedTime)
 
-    // animate sphere
-
-    sphere.position.z = -Math.sin(elapsedTime)
-    sphere.position.x = -Math.sin(elapsedTime)
-    sphere.position.y = -Math.sin(elapsedTime)
 
     // Update Orbit Controls
     controls.update()
